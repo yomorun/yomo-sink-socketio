7eaf3ec2bae0cb618a8dcb5e4ecd0f1937b7c239
Hong Xiaojian <xiaojian.hong@gmail.com>
2020-12-30 19:09:22 +0800

    feat: add socket.io server and sink server

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/yomorun/yomo-sink-socketio
+
+go 1.15
+
+require (
+	github.com/googollee/go-socket.io v1.4.4
+	github.com/yomorun/yomo v0.5.2
+)
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+	"github.com/yomorun/yomo/pkg/quic"
+	"github.com/yomorun/yomo/pkg/rx"
+)
+
+const (
+	socketioRoom   = "yomo-demo"
+	socketioAddr   = "0.0.0.0:8000"
+	sinkServerAddr = "0.0.0.0:4141"
+)
+
+func main() {
+	socketioServer, err := newSocketIOServer()
+	if err != nil {
+		log.Printf("❌ Initialize the socket.io server failure with err: %v", err)
+		return
+	}
+
+	// sink server which will receive the data from `yomo-sink`.
+	go serveSinkServer(socketioServer, sinkServerAddr)
+
+	// serve socket.io server.
+	go socketioServer.Serve()
+	defer socketioServer.Close()
+
+	http.Handle("/socket.io/", socketioServer)
+	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Print("✅ Serving socket.io on ", socketioAddr)
+	err = http.ListenAndServe(socketioAddr, nil)
+	if err != nil {
+		log.Printf("❌ Serving the socket.io server on %s failure with err: %v", socketioAddr, err)
+		return
+	}
+}
+
+func newSocketIOServer() (*socketio.Server, error) {
+	log.Print("Starting socket.io server...")
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// add all connected user to the room "yomo-demo".
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		log.Print("connected:", s.ID())
+		s.Join(socketioRoom)
+
+		return nil
+	})
+
+	return server, nil
+}
+
+// serveSinkServer serves the Sink server over QUIC.
+func serveSinkServer(socketioServer *socketio.Server, addr string) {
+	log.Print("Starting sink server...")
+	quicHandler := &quicServerHandler{
+		socketioServer,
+	}
+	quicServer := quic.NewServer(quicHandler)
+
+	err := quicServer.ListenAndServe(context.Background(), addr)
+	if err != nil {
+		log.Printf("❌ Initialize the socket.io server failure with err: %v", err)
+	}
+}
+
+type quicServerHandler struct {
+	socketioServer *socketio.Server
+}
+
+func (s *quicServerHandler) Listen() error {
+	// you can add the customized codes which will be triggered when QUIC server is listening.
+	return nil
+}
+
+func (s *quicServerHandler) Read(st quic.Stream) error {
+	// use rx (ReactiveX) to process the stream
+	// and decode the data via Y3 Codec.
+	rxStream := rx.FromReader(st).Y3Decoder("0x10", float32(0)).StdOut()
+	go func() {
+		for customer := range rxStream.Observe() {
+			if customer.Error() {
+				log.Print(customer.E.Error())
+			} else if customer.V != nil {
+				// broadcast message to all connected user.
+				s.socketioServer.BroadcastToRoom("", socketioRoom, "receive_sink", customer.V)
+			}
+			st.Write([]byte("Received data."))
+		}
+	}()
+
+	return nil
+}
